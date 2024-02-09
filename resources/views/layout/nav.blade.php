6bee60c55f15eefbf099dc49366a0cc15574fd48
NiteshHamal <nitesh0hamal@gmail.com>
2024-02-09 16:29:43 +0545

    commit ui change

--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -52,7 +52,7 @@
                         </div>
                         <!-- Header User End -->
                         <!-- Header Cart Start -->
-                        <a href="wishlist.html" class="ec-header-btn ec-header-wishlist">
+                        <a href="{{ url('wishlist') }}" class="ec-header-btn ec-header-wishlist">
                             <div class="header-icon"><i class="fi-rr-heart"></i></div>
                             <span class="ec-header-count">4</span>
                         </a>
