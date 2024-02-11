462c4012223524b8059dd275fc001509d1bc96ad
Hancie123 <hanciewanemphago@gmail.com>
2024-02-11 15:06:43 +0545

    map change

--- resources/views/home/about_banner.blade.php
+++ resources/views/home/about_banner.blade.php
@@ -4,13 +4,19 @@
         <div class="row">
             <div class="col-md-6">
                 <div class="ec-about-img">
-                    <img src="{{ url('assets/img/pot.png') }}" alt="about-image">
+                    <iframe width="100%" height="315"
+                        src="https://www.youtube.com/embed/swfHcNIAq8A?si=laCIBgSB34memaKx" title="YouTube video player"
+                        frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        allowfullscreen></iframe>
                 </div>
             </div>
+
             <div class="col-md-6">
                 <div class="ec-about-detail">
                     <h4>Gamala Ghar</h4>
-                    <p class="detail-txt m-b-20">Gamala Ghar is an innovative ecommerce platform dedicated to providing a
+                    <p class="detail-txt m-b-20">Gamala Ghar is an innovative ecommerce platform dedicated to providing
+                        a
                         wide range of indoor plants to enhance the ambiance and freshness of homes. With a focus on
                         promoting well-being and creating healthier living spaces, Gamala Ghar offers a curated
                         selection of indoor plants that are not only visually appealing but also contribute to improving
@@ -21,8 +27,7 @@
                         <div>
                             <h3 class="about-detail-text">Enhanced Well-being</h3>
                             <p class="detail-txt">Indoor plants have been shown to boost mood, reduce stress, and
-                                increase creativity. By bringing nature indoors, Gamala Ghar helps create a healthier
-                                and more positive living environment for its customers.</p>
+                                increase creativity. </p>
                         </div>
                     </div>
                     <div class="ec-detail-list">
@@ -31,9 +36,7 @@
                         <div>
                             <h3 class="about-detail-text">Improved Indoor Air Quality</h3>
                             <p class="detail-txt">Indoor plants act as natural air purifiers, filtering out harmful
-                                toxins and pollutants from the air. By introducing a variety of indoor plants into
-                                homes, Gamala Ghar helps to eliminate indoor air pollutants, such as benzene,
-                                formaldehyde, and trichloroethylene.</p>
+                                toxins and pollutants from the air.</p>
                         </div>
                     </div>
                 </div>
