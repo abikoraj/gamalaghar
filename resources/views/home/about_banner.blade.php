29f2158945692703b010230b0449bbbbbba3ec97
Hancie123 <hanciewanemphago@gmail.com>
2024-02-11 14:59:42 +0545

    map change

--- resources/views/home/about_banner.blade.php
+++ resources/views/home/about_banner.blade.php
@@ -4,37 +4,36 @@
         <div class="row">
             <div class="col-md-6">
                 <div class="ec-about-img">
-                    <img src="assets/images/about/about2.png" alt="about-image">
+                    <img src="{{ url('assets/img/pot.png') }}" alt="about-image">
                 </div>
             </div>
             <div class="col-md-6">
                 <div class="ec-about-detail">
-                    <h4>We Are Healthy Food</h4>
-                    <p class="detail-txt m-b-20">Lorem Ipsum is simply dummy text of the printing and typesetting
-                        industry.
-                        Lorem Ipsum has
-                        been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
-                        galley of type and scrambled it to make a type specimen book.</p>
+                    <h4>Gamala Ghar</h4>
+                    <p class="detail-txt m-b-20">Gamala Ghar is an innovative ecommerce platform dedicated to providing a
+                        wide range of indoor plants to enhance the ambiance and freshness of homes. With a focus on
+                        promoting well-being and creating healthier living spaces, Gamala Ghar offers a curated
+                        selection of indoor plants that are not only visually appealing but also contribute to improving
+                        indoor air quality and overall mood.</p>
                     <div class="ec-detail-list m-b-15">
-                        <div class="detail-list-img"><img src="assets/images/about/vegan.png" alt="plant-img">
+                        <div class="detail-list-img"><img src="{{ url('assets/img/plant.png') }}" alt="plant-img">
                         </div>
                         <div>
-                            <h3 class="about-detail-text">Biological Benefits</h3>
-                            <p class="detail-txt">Lorem Ipsum is simply dummy text of the printing and typesetting
-                                industry. Lorem
-                                Ipsum
-                                has been the industry's .</p>
+                            <h3 class="about-detail-text">Enhanced Well-being</h3>
+                            <p class="detail-txt">Indoor plants have been shown to boost mood, reduce stress, and
+                                increase creativity. By bringing nature indoors, Gamala Ghar helps create a healthier
+                                and more positive living environment for its customers.</p>
                         </div>
                     </div>
                     <div class="ec-detail-list">
-                        <div class="detail-list-img"><img src="assets/images/about/plant.png" alt="plant-img">
+                        <div class="detail-list-img"><img src="{{ url('assets/img/plant.png') }}" alt="plant-img">
                         </div>
                         <div>
-                            <h3 class="about-detail-text">Speciality Product</h3>
-                            <p class="detail-txt">Lorem Ipsum is simply dummy text of the printing and typesetting
-                                industry. Lorem
-                                Ipsum
-                                has been the industry's .</p>
+                            <h3 class="about-detail-text">Improved Indoor Air Quality</h3>
+                            <p class="detail-txt">Indoor plants act as natural air purifiers, filtering out harmful
+                                toxins and pollutants from the air. By introducing a variety of indoor plants into
+                                homes, Gamala Ghar helps to eliminate indoor air pollutants, such as benzene,
+                                formaldehyde, and trichloroethylene.</p>
                         </div>
                     </div>
                 </div>
