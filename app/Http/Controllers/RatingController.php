27fcdf8f00e937362b3b5da72f7a0506d4abc758
NiteshHamal <nitesh0hamal@gmail.com>
2024-02-29 15:08:05 +0545

    commit change

--- app/Http/Controllers/RatingController.php
+++ app/Http/Controllers/RatingController.php
@@ -3,11 +3,29 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\Rating\RatingStoreRequest;
+use App\Models\Rating;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class RatingController extends Controller
 {
-    public function store(RatingStoreRequest $request){
-
+    public function store(RatingStoreRequest $request)
+    {
+        try {
+            $rating = DB::transaction(function () use ($request) {
+                $rating = Rating::create([
+                    'product_id' => $request->product_id,
+                    'user_id' => auth()->user()->id,
+                    'rating' => $request->rating,
+                    'comment' => $request->comment,
+                ]);
+                return $rating;
+            });
+            if ($rating) {
+                return response()->json(['message' => 'Rating saved!']);
+            }
+        } catch (\Exception $e) {
+            return response()->json(['message'=>$e->getMessage()]);
+        }
     }
 }
