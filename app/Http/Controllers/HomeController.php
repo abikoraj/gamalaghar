6da7e0aa19c40fd36a13f161e06dc5213b06dfb7
NiteshHamal <nitesh0hamal@gmail.com>
2024-03-01 16:05:19 +0545

    faq seen accoridng to db

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Cart;
+use App\Models\Faq;
 use App\Models\MainCategory;
 use App\Models\Product;
 use App\Models\SubCategory;
@@ -19,20 +20,22 @@ class HomeController extends Controller
             $countWishList = Wishlist::where('user_id', auth()->user()->id)->count();
             $countCarts = Cart::where('user_id', auth()->user()->id)->count();
             $cart = Cart::join('products', 'products.id', '=', 'carts.product_id')
-            ->join('product_size_prices', 'product_size_prices.id', '=', 'carts.product_size_price_id')
-            ->join('sizes', 'sizes.id', '=', 'product_size_prices.size_id')
-            ->select('products.id', 'products.product_name', 'products.slug', 'product_size_prices.price', 'sizes.size', 'carts.quantity', 'carts.id as cartid', 'carts.user_id')
-            ->groupBy('cartid', 'products.id', 'products.product_name', 'products.slug', 'product_size_prices.price', 'sizes.size', 'carts.quantity', 'carts.user_id')
-            ->where('carts.user_id', auth()->user()->id)->get();
+                ->join('product_size_prices', 'product_size_prices.id', '=', 'carts.product_size_price_id')
+                ->join('sizes', 'sizes.id', '=', 'product_size_prices.size_id')
+                ->select('products.id', 'products.product_name', 'products.slug', 'product_size_prices.price', 'sizes.size', 'carts.quantity', 'carts.id as cartid', 'carts.user_id')
+                ->groupBy('cartid', 'products.id', 'products.product_name', 'products.slug', 'product_size_prices.price', 'sizes.size', 'carts.quantity', 'carts.user_id')
+                ->where('carts.user_id', auth()->user()->id)->get();
             $productId = $cart->pluck('id')->toArray();
             $cartproductImages = Product::with('media')->whereIn('id', $productId)->get();
         } else {
             $countWishList = "";
             $countCarts = "";
-            $cart=[];
-            $cartproductImages=[];
+            $cart = [];
+            $cartproductImages = [];
             $mainCategory = MainCategory::with('subcategories')->get();
         }
-        return view('home.home', compact('mainCategory', 'product', 'countWishList', 'cart', 'cartproductImages', 'countCarts'));
+
+        $faqs = Faq::all();
+        return view('home.home', compact('mainCategory', 'product', 'countWishList', 'cart', 'cartproductImages', 'countCarts', 'faqs'));
     }
 }
--- resources/views/home/faq.blade.php
+++ resources/views/home/faq.blade.php
@@ -9,54 +9,26 @@
                             important information about us</h3>
                     </div>
                     <div class="accordion" id="accordion">
-                        <div class="accordion-item">
-                            <h2 class="accordion-header" id="headingOne">
-                                <button class="accordion-button" type="button" data-bs-toggle="collapse"
-                                    data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                    There are many variations of passages of Lorem Ipsum available,
-                                </button>
-                            </h2>
-                            <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne"
-                                data-bs-parent="#accordion">
-                                <div class="accordion-body">
-                                    Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots
-                                    in a
-                                    piece of classical Latin literature from 45 BC, making it over 2000 years old.
+
+                        @foreach ($faqs as $faq)
+                            <div class="accordion-item">
+                                <h2 class="accordion-header" id="heading{{ $faq->id }}">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                        data-bs-target="#collapse{{ $faq->id }}" aria-expanded="false" aria-controls="collapse{{ $faq->id }}">
+                                        {{ $faq->question }}
+                                    </button>
+                                </h2>
+                                <div id="collapse{{ $faq->id }}" class="accordion-collapse collapse" aria-labelledby="heading{{ $faq->id }}"
+                                    data-bs-parent="#accordion">
+                                    <div class="accordion-body">
+                                        {{ $faq->answer }}
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="accordion-item">
-                            <h2 class="accordion-header" id="headingTwo">
-                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                                    data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                                    There are many variations of passages of Lorem Ipsum available,
-                                </button>
-                            </h2>
-                            <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo"
-                                data-bs-parent="#accordion">
-                                <div class="accordion-body">
-                                    Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots
-                                    in a
-                                    piece of classical Latin literature from 45 BC, making it over 2000 years old.
-                                </div>
-                            </div>
-                        </div>
-                        <div class="accordion-item">
-                            <h2 class="accordion-header" id="headingThree">
-                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                                    data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                                    There are many variations of passages of Lorem Ipsum available,
-                                </button>
-                            </h2>
-                            <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree"
-                                data-bs-parent="#accordion">
-                                <div class="accordion-body">
-                                    Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots
-                                    in a
-                                    piece of classical Latin literature from 45 BC, making it over 2000 years old.
-                                </div>
-                            </div>
-                        </div>
+                        @endforeach
+
+
+
                     </div>
                 </div>
             </div>
